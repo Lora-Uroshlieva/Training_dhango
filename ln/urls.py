bdd7353bcb4b64da314876ab8bfcad831bb694eb
order12a <vladimir.uroshlev@gmail.com>
2017-02-07 17:21:12 +0200

    Templates (login.html, base.html) were added. Urls.py was changed.

--- ln/urls.py
+++ ln/urls.py
@@ -20,9 +20,12 @@ from django.contrib.auth import views as auth_views
 from ln import views
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^accounts/login/$', auth_views.login),
     url(r'^$', views.dashboard, name='dashboard'),
+    url(r'^accounts/login/$', auth_views.login, name='login'),
+    url(r'^accounts/logout/$', auth_views.logout, name='logout'),
+    url(r'^accounts/password_reset/$', auth_views.password_reset, name='password_reset'),
+    url(r'^accounts/password_reset_done/$', auth_views.password_reset_done, name='password_reset_done'),
+    url(r'^admin/', admin.site.urls),
     url(r'^movie/order/(?P<movie_id>\d+)$', views.movie_order,
         name='movie_order'),
 ]
