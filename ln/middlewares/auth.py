0d3d99750e1dccd0c5d948d82bf104b22ed29d11
lora.strizhakova@gmail.com <rfrnecyzr9802>
2017-02-10 20:10:53 +0200

    auth.py was changed

--- ln/middlewares/auth.py
+++ ln/middlewares/auth.py
@@ -13,25 +13,6 @@ class Auth(object):
         except KeyError:
             pass
 
-        print (request.COOKIES['user-session'])
-        user_session = request.COOKIES
         response = self.get_response(request)
 
         return response
-
-
-# class Auth(object):
-#     def __init__(self, get_response):
-#         self.get_response = get_response
-#         # One-time configuration and initialization.
-#
-#     def __call__(self, request):
-#         # Code to be executed for each request before
-#         # the view (and later middleware) are called.
-#
-#         response = self.get_response(request)
-#
-#         # Code to be executed for each request/response after
-#         # the view is called.
-#
-#         return response
